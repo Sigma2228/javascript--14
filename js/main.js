f3c1a33f6d1ddb9518c7c2262007ee817aa21c98
Sigma2228 <149525498+Sigma2228@users.noreply.github.com>
2025-06-28 17:43:40 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
+// завдання 1
 function delayedPromise(value, delay) {
     return new Promise(resolve => {
-        setTimeout(() => resolve(value));
-    }, delay)
+        setTimeout(() => resolve(value), delay);
+    })
    }
-   const promises = [
-    delayedPromise('A', 1000),
-    delayedPromise('B', 500),
-    delayedPromise('C', 1500),
-    delayedPromise('D', 700),
-    delayedPromise('E', 1200)
-];
+
+
+   const promises = [delayedPromise('A', 1000), delayedPromise('B', 500), delayedPromise('C', 1500), delayedPromise('D', 700), delayedPromise('E', 1200)];
+
+
 Promise.all(promises).then(result => {
        console.log('Результати:', result)
+})
+
+// завдання 2
+function randomDelay(value) {
+    const delay = Math.floor(Math.random() * 4001) + 1000;
+    return new Promise(resolve => {
+        setTimeout(() => resolve(value), delay)
+    })
+}
+
+
+const promisess = [randomDelay('4'), randomDelay('2'), randomDelay('8'), randomDelay('3'), randomDelay('1')]
+Promise.race(promisess).then(result => {
+    console.log(result)
 })
